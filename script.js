27307de31d05aef0a446a785a8a37e157938a58b
Nino Chimento <saninoc84@gmail.com>
2020-02-06 10:07:53 +0100

    inserimento if con lunghezza 0 ricerca

--- script.js
+++ script.js
@@ -2,22 +2,28 @@
 $(document).ready(function () {
   $("button").click(function () {
     var ricerca = $("input").val();
-    $.ajax({
-      url : "https://api.themoviedb.org/3/search/movie",
-      method : "GET",
-      data : {
-        api_key :"5ae01722ee057b2fdbbf926a615150da",
-        query : ricerca,
-        language:"it-IT"
-      },
-      success : function (data) {
-        var listaFilms = data.results;
-        stampaFilms(listaFilms)
-      },
-      error : function (request,state,error) {
-        alert("errore e"+errore)
-      }
-    });
+    if (ricerca.length == 0) {
+      alert("inserisci una parola chiave grazie")
+    }
+    else {
+      $.ajax({
+        url : "https://api.themoviedb.org/3/search/movie",
+        method : "GET",
+        data : {
+          api_key :"5ae01722ee057b2fdbbf926a615150da",
+          query : ricerca,
+          language:"it-IT"
+        },
+        success : function (data) {
+          var listaFilms = data.results;
+          stampaFilms(listaFilms)
+        },
+        error : function (request,state,error) {
+          alert("errore e"+errore)
+        }
+      });
+
+    }
   });
 });
 // funzione stampa film
