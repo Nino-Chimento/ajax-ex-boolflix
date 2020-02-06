32a665cf9aceea52a1fe80f7c1ff2dfcd7ecf8f8
Nino Chimento <saninoc84@gmail.com>
2020-02-06 09:43:30 +0100

    faccio uan chiamata ajax e stampo a video la lista film

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 $(document).ready(function () {
   $("button").click(function () {
     var ricerca = $("input").val();
-    console.log(ricerca);
+    $.ajax({
+      url : "https://api.themoviedb.org/3/search/movie",
+      method : "GET",
+      data : {
+        api_key :"5ae01722ee057b2fdbbf926a615150da",
+        query : ricerca,
+        language:"it-IT"
+      },
+      success : function (data) {
+        var listaFilms = data.results;
+      },
+      error : function (request,state,error) {
+        alert("errore e"+errore)
+      }
+    });
   });
 });
